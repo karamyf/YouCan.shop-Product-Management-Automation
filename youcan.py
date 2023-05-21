4530e0f904fd9da465dc234616b96ad75ead3a82
karamyf <karam_youssef@emsi-edu.ma>
2023-05-21 01:41:30 +0100

    from main to UI

--- youcan.py
+++ youcan.py
@@ -1,17 +1,20 @@
+import os
 import time
-from selenium.webdriver.common.keys import Keys
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
 import pandas as pd
-import credentials
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import Select
-import os
+from tkinter import Tk, Label, Button, Entry, StringVar
 
-#login method
+
+import credentials
+
+# Login method
 def login():
     # Set up the webdriver
     driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -35,13 +38,13 @@ def login():
 
     return driver
 
-#add products from excel sheet
-def add_product(driver):
+# Add products from an Excel sheet
+def add_product(driver, product_name):
     # Navigate to the product creation page
     driver.get('https://seller-area.youcan.shop/admin/products/create?')
 
     # Read the Excel file
-    df = pd.read_excel('products moha.xlsx', skiprows=39)
+    df = pd.read_excel(f'C:\\Users\\pc\\Desktop\\Home\\Work\\Fiverr\\Customers\\Bilal Hagouch\\Products\\{product_name}\\reviews\\reviews.xlsx')
 
     # Iterate over the rows in the Excel file
     for i, row in df.iterrows():
@@ -64,7 +67,6 @@ def add_product(driver):
         # Navigate back to the product creation page for the next product
         driver.get('https://seller-area.youcan.shop/admin/products/create?')
 
-#add reviews from excel sheet
 # Add reviews from an Excel sheet
 def add_review(driver, product_name):
     driver.get("https://seller-area.youcan.shop/admin/products/reviews")
@@ -111,7 +113,6 @@ def add_review(driver, product_name):
         driver.find_element(By.CSS_SELECTOR, ".button").click()
         driver.find_element(By.CSS_SELECTOR, "tr:nth-child(1) label").click()
 
-
 # Create folders for a new product
 def new_product(client_name, product_name):
     base_path = f"C:\\Users\\pc\\Desktop\\Home\\Work\\Fiverr\\Customers\\{client_name}\\Products\\{product_name}"
@@ -128,34 +129,93 @@ def new_product(client_name, product_name):
 
     print(f"New product '{product_name}' for client '{client_name}' created successfully.")
 
+# Main menu
 
-
+'''
 def main():
+    driver = None
 
-    print("Menu:")
-    print("1 - Add product")
-    print("2 - Add review")
-    print("3 - Create new product")
-    choice = input("Enter your choice: ")
+    try:
+        
 
-    if choice == "1":
-        driver = login()
-        add_product(driver)
-    elif choice == "2":
-        driver = login()
-        product_name = input("Enter the product name: ")
-        add_review(driver, product_name)
-    elif choice == "3":
-        client_name = input("Enter the client name: ")
-        product_name = input("Enter the product name: ")
-        new_product(client_name, product_name)
-    
-    else:
-        print("Invalid choice")
+        print("Menu:")
+        print("1 - Add product")
+        print("2 - Add review")
+        print("3 - Create new product")
+        choice = input("Enter your choice: ")
 
-    # Close the webdriver
-    driver.quit()
+        if choice == "1":
+            driver = login()
+            product_name = input("Enter the product name: ")
+            add_product(driver, product_name)
+        elif choice == "2":
+            product_name = input("Enter the product name: ")
+            driver = login()
+            add_review(driver, product_name)
+        elif choice == "3":
+            client_name = input("Enter the client name: ")
+            product_name = input("Enter the product name: ")
+            new_product(client_name, product_name)
+        else:
+            print("Invalid choice")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+    finally:
+        if driver is not None:
+            # Close the webdriver
+            driver.quit()
 
 
 if __name__ == "__main__":
     main()
+'''
+
+# Function to handle the button click event for adding a product
+def add_product_button_click():
+    product_name = product_entry.get()
+    if product_name:
+        driver = login()
+        add_product(driver, product_name)
+        driver.quit()
+
+# Function to handle the button click event for adding a review
+def add_review_button_click():
+    product_name = product_entry.get()
+    if product_name:
+        driver = login()
+        add_review(driver, product_name)
+        driver.quit()
+
+# Function to handle the button click event for creating a new product
+def create_product_button_click():
+    client_name = client_entry.get()
+    product_name = product_entry.get()
+    if client_name and product_name:
+        new_product(client_name, product_name)
+
+# Create the GUI
+root = Tk()
+root.title("YouCan.shop Product Management")
+
+# Labels
+client_label = Label(root, text="Client Name:")
+client_label.grid(row=0, column=0, padx=10, pady=10)
+product_label = Label(root, text="Product Name:")
+product_label.grid(row=1, column=0, padx=10, pady=10)
+
+# Entry fields
+client_entry = Entry(root)
+client_entry.grid(row=0, column=1, padx=10, pady=10)
+product_entry = Entry(root)
+product_entry.grid(row=1, column=1, padx=10, pady=10)
+
+# Buttons
+add_product_button = Button(root, text="Add Product", command=add_product_button_click)
+add_product_button.grid(row=2, column=0, padx=10, pady=10)
+add_review_button = Button(root, text="Add Review", command=add_review_button_click)
+add_review_button.grid(row=2, column=1, padx=10, pady=10)
+create_product_button = Button(root, text="Create New Product", command=create_product_button_click)
+create_product_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+
+# Start the GUI main loop
+root.mainloop()
