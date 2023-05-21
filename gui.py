d56dcc75f6f5a2cd861d4e1af50bb6e63d91ab5a
karamyf <karam_youssef@emsi-edu.ma>
2023-05-21 18:30:40 +0100

    set up dynamic workflow

--- gui.py
+++ gui.py
@@ -4,24 +4,24 @@ from login import login
 import os
 
 # Change your workflow
-workflow = "C:\\Users\\pc\\Desktop\\Home\\Work\\Fiverr\\Customers\\"
+workflow = "C:\\Users\\pc\\Desktop\\Home\\Work\\Fiverr\\Customers"
 
 # Create the GUI
 root = Tk()
 root.title("YouCan Product Management")
 
 # Styling
-root.geometry("400x300")
-root.configure(bg="#f5f5f5")
+root.geometry("500x500")
+root.configure(bg="#212121")
 
 label_font = ("Arial", 12, "bold")
 button_font = ("Arial", 10)
 entry_font = ("Arial", 10)
 
 # Labels
-client_label = Label(root, text="Client Name:", font=label_font, bg="#f5f5f5")
+client_label = Label(root, text="Client Name:", font=label_font, bg="#212121", fg="#ffffff")
 client_label.grid(row=0, column=0, padx=10, pady=10)
-product_label = Label(root, text="Product Name:", font=label_font, bg="#f5f5f5")
+product_label = Label(root, text="Product Name:", font=label_font, bg="#212121", fg="#ffffff")
 product_label.grid(row=1, column=0, padx=10, pady=10)
 
 # Dropdown menu variables
@@ -30,11 +30,11 @@ selected_product = StringVar(root)
 
 # Dropdown menus
 clients_menu = OptionMenu(root, selected_client, "")
-clients_menu.config(font=entry_font)
+clients_menu.config(font=entry_font, bg="#424242", fg="#ffffff")
 clients_menu.grid(row=0, column=1, padx=10, pady=10, sticky="w")
 
 products_menu = OptionMenu(root, selected_product, "")
-products_menu.config(font=entry_font)
+products_menu.config(font=entry_font, bg="#424242", fg="#ffffff")
 products_menu.grid(row=1, column=1, padx=10, pady=10, sticky="w")
 
 # Update products menu based on selected client
@@ -71,7 +71,7 @@ def search_entries():
 # Search entry field
 search_entry = Entry(root, font=entry_font)
 search_entry.grid(row=2, column=0, padx=10, pady=10, sticky="w")
-search_button = Button(root, text="Search", command=search_entries, font=button_font)
+search_button = Button(root, text="Search", command=search_entries, font=button_font, bg="#757575", fg="#ffffff")
 search_button.grid(row=2, column=1, padx=10, pady=10, sticky="w")
 
 # Buttons
@@ -97,11 +97,11 @@ def create_product_button_click():
     if client_name and product_name:
         new_product(client_name, product_name)
 
-add_product_button = Button(root, text="Add Product", command=add_product_button_click, font=button_font)
+add_product_button = Button(root, text="Add Product", command=add_product_button_click, font=button_font, bg="#1976D2", fg="#ffffff", bd=0, padx=10, pady=5, relief="solid", borderwidth=0, highlightthickness=0, highlightbackground="#212121", highlightcolor="#212121", activebackground="#1565C0", activeforeground="#ffffff")
 add_product_button.grid(row=3, column=0, padx=10, pady=10)
-add_review_button = Button(root, text="Add Review", command=add_review_button_click, font=button_font)
+add_review_button = Button(root, text="Add Review", command=add_review_button_click, font=button_font, bg="#1976D2", fg="#ffffff", bd=0, padx=10, pady=5, relief="solid", borderwidth=0, highlightthickness=0, highlightbackground="#212121", highlightcolor="#212121", activebackground="#1565C0", activeforeground="#ffffff")
 add_review_button.grid(row=3, column=1, padx=10, pady=10)
-create_product_button = Button(root, text="Create New Product", command=create_product_button_click, font=button_font)
+create_product_button = Button(root, text="Create New Product", command=create_product_button_click, font=button_font, bg="#1976D2", fg="#ffffff", bd=0, padx=10, pady=5, relief="solid", borderwidth=0, highlightthickness=0, highlightbackground="#212121", highlightcolor="#212121", activebackground="#1565C0", activeforeground="#ffffff")
 create_product_button.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
 
 # Start the GUI main loop
