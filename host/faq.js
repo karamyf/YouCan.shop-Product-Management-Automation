f697c08897ea017f39744d453e6e6cd83a68bf10
karamyf <karam_youssef@emsi-edu.ma>
2023-07-24 15:12:40 +0100

    update faq

--- host/faq.js
+++ host/faq.js
@@ -1,25 +1,24 @@
+  $(document).ready(function() {
+    // List of allowed website domains
+    const allowedDomains = ["souqplus.youcan.shop", "website1.com", "website2.com"];
 
-const allowedDomains = ["souqplus.com", "website1.com", "website2.com"];
+    // Get the current website's domain
+    const currentDomain = window.location.hostname;
 
-$(document).ready(function() {
-  // Get the current website's domain
-  const currentDomain = window.location.hostname;
-
-  if (allowedDomains.includes(currentDomain)) {
-    // Add click event to the FAQ questions
-    $(".faq-question").click(function() {
-      var s = $(this).next(".faq-answer");
-      if ($(this).hasClass("uncollapsed")) {
-        s.slideUp();
-        $(this).removeClass("uncollapsed");
-      } else {
-        $(".faq-question.uncollapsed")
-          .next(".faq-answer")
-          .slideUp();
-        $(".faq-question.uncollapsed").removeClass("uncollapsed");
-        s.slideDown();
-        $(this).addClass("uncollapsed");
-      }
-    });
-  }
-});
+    if (allowedDomains.includes(currentDomain)) {
+      $(".faq-question").click(function() {
+        var s = $(this).next(".faq-answer");
+        if ($(this).hasClass("uncollapsed")) {
+          s.slideUp();
+          $(this).removeClass("uncollapsed");
+        } else {
+          $(".faq-question.uncollapsed")
+            .next(".faq-answer")
+            .slideUp();
+          $(".faq-question.uncollapsed").removeClass("uncollapsed");
+          s.slideDown();
+          $(this).addClass("uncollapsed");
+        }
+      });
+    }
+  });
